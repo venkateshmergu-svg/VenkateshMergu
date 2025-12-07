860bcaff4ecfef129d1787103daafe6d818cf500
Venkatesh Mergu <your_email@example.com>
2025-12-07 15:32:06 +0530

    Added addition_program.py

---
+++ addition_program.py
@@ -0,0 +1,11 @@
+# Program for addition of two numbers
+
+# Input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {sum}")
